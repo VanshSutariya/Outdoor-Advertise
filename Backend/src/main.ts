733f3ae223fcc1d95c874dfd7489eda65cca3582
vanshsutariya <sutariyavansh@gmail.com>
2024-10-15 22:36:08 +0530

    fixes.

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,21 +3,33 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import helmet from 'helmet';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-    cors: true,
-  });
-  app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  };
+let appInstance = null;
 
-  app.enableCors(corsOptions);
+async function createNestApp() {
+  if (!appInstance) {
+    const app = await NestFactory.create(AppModule, {
+      rawBody: true,
+      cors: true,
+    });
+    app.use(helmet());
+    app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(4000);
+    const corsOptions: CorsOptions = {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    };
+    app.enableCors(corsOptions);
+
+    await app.init(); // Initialize the app instead of listening
+    appInstance = app.getHttpAdapter().getInstance();
+  }
+  return appInstance;
 }
-bootstrap();
+
+// This is the handler Vercel will use
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const app = await createNestApp();
+  app(req, res); // Pass the request and response to the NestJS app
+}
