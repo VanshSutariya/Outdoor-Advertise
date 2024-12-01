3427a597dbc6829d305afdd060eba551794e5e00
vanshsutariya <sutariyavansh@gmail.com>
2024-12-01 12:31:31 +0530

    main ts file fixes.

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,23 +1,46 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+// import helmet from 'helmet';
+
+// async function createNestApp() {
+//   const app = await NestFactory.create(AppModule, {
+//     rawBody: true,
+//     cors: true,
+//   });
+//   app.use(helmet());
+//   app.useGlobalPipes(new ValidationPipe());
+
+//   const corsOptions: CorsOptions = {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   };
+//   app.enableCors(corsOptions);
+
+//   await app.listen(4000)
+// }
+// createNestApp()
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
-async function createNestApp() {
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-    cors: true,
-  });
-  app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
+let cachedApp;
 
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  };
-  app.enableCors(corsOptions);
+export async function handler(event, context) {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule);
+    app.use(helmet());
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    });
+    await app.init();
+    cachedApp = app.getHttpAdapter().getInstance();
+  }
 
-  await app.listen(4000)
+  return cachedApp(event, context);
 }
-createNestApp()
